b302522dcfd9d5ccf2a86dd87d143fa0ba0a81e3
Yunseo Park <yunseo2815@naver.com>
2025-05-12 22:25:23 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -0,0 +1,25 @@
+with july_order as
+(
+    select flavor
+        , sum(total_order) as july_cnt
+    from july
+    group by 1
+)
+, total_order as
+(
+    select j.flavor
+        , (july_cnt + total_order) as total_cnt
+    from july_order as j
+    join first_half as f on j.flavor = f.flavor
+    group by 1
+)
+, rnk as
+(
+    select flavor
+        , rank() over (order by total_cnt desc) as rnk
+    from total_order
+)
+select flavor
+from rnk
+where 1=1
+    and rnk <= 3
