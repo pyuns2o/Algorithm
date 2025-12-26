e5d9c924a983c67e817c9caa9ef84012f73b27c7
Yunseo Park <yunseo2815@naver.com>
2025-12-26 15:09:28 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,15 @@
 from collections import Counter
 
 def solution(clothes):
-    type_counter = Counter([kind for name, kind in clothes])
-    
     answer = 1
-    
-    for count in type_counter.values():
-        answer *= (count + 1)
-        
-    return answer - 1
+    c_list = []
+    # 의상종류만 담긴 리스트를 새로 파
+    for item in clothes:
+        c_list.append(item[1])
+    # counter로 의상 종류별로 갯수를 세
+    clo_count = Counter(c_list)
+    # 개수 + 1씩 곱해 (안 입는 경우의 수가 있는거니까)
+    for cnt in clo_count.values():
+        answer = answer * (cnt+1)
+    # 그다음에 안 입는 경우는 없으니까 -1 해줘
+    return answer -1
