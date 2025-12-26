13b3d56e57c9b68a9742e5bd99ee76e9e31c5755
Yunseo Park <yunseo2815@naver.com>
2025-12-26 10:33:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.83 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,11 +2,10 @@ from collections import Counter
 
 def solution(participant, completion):
     answer = ''
-    p_dict = Counter(participant)
-    c_dict = Counter(completion) 
+    c_par = Counter(participant)
+    c_com = Counter(completion)
     
-    for p_name in p_dict:
-        if p_dict[p_name] != c_dict[p_name]:
+    for p_name in c_par:
+        if c_par[p_name] != c_com[p_name]:
             answer = p_name
-
     return answer
