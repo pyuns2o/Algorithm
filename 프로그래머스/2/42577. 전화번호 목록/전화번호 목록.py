cb8282ead05479d2003e33a43e7c50147d863d52
Yunseo Park <yunseo2815@naver.com>
2025-12-26 11:31:43 +0900

    [level 2] Title: 전화번호 목록, Time: 107.22 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,7 @@
 def solution(phone_book):
+    answer = True
     phone_book.sort()
-    for i in range(len(phone_book) -1):
-        if phone_book[i+1].startswith(phone_book[i]):
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]): # 바로 옆옆만 비교할 수 있는 코드잖아...
             return False
-    return True
+    return answer
