57dbebaca2b653276a95b690fc3327a0141e2209
Yunseo Park <yunseo2815@naver.com>
2025-12-28 19:31:32 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 125.59 ms, Memory: 27 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,6 +1,13 @@
+# 아하 그냥 중복을 제거하면 되는 문제가 아니구나
+# 연속일 경우!!
+
 def solution(arr):
-    answer = [arr[0]]
-    for i in range(1, len(arr)):
-        if arr[i] != arr[i-1]:
+    answer = []
+    for i in range(len(arr)):
+        if i == 0:
+            answer.append(arr[i])
+        elif arr[i-1] == arr[i]:
+            continue
+        else:
             answer.append(arr[i])
     return answer
