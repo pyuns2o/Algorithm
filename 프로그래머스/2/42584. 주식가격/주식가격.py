227a4ff9a7b24ea64392b75d2fe665e768e1b95b
Yunseo Park <yunseo2815@naver.com>
2025-12-29 18:20:03 +0900

    [level 2] Title: 주식가격, Time: 102.51 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,20 +1,15 @@
+from collections import deque
+
 def solution(prices):
-    n = len(prices)
-    answer = [0] * n
-    stack = []
+    queue = deque(prices)
+    answer = []
     
-    for i, price in enumerate(prices):
-        # 가격이 떨어졌다면
-        while stack and price < prices[stack[-1]]:
-            prev_i = stack.pop()
-            answer[prev_i] = i - prev_i # 지금 시점 - 이전 시점 = 버틴 시간
-            
-        # 떨어지지 않은 경우 스택에 넣어주기
-        stack.append(i)
-    
-    # 끝까지 떨어지지 않고 스택에 쌓여있는 애들 처리
-    for i in stack:
-        answer[i] = n - 1 - i # 마지막 index - 현재 index
-        
-        
+    while queue:
+        price = queue.popleft()
+        sec = 0
+        for q in queue:
+            sec += 1
+            if price > q:
+                break 
+        answer.append(sec)        
     return answer
