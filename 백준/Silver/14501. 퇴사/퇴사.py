09a2075c70b2fd1ed8dee00d7ad9c357b2fb7457
Yunseo Park <yunseo2815@naver.com>
2026-01-04 23:26:38 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+T = [0] * (n + 1)
+P = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    T[i], P[i] = map(int, input().split())
+
+# dp[i] = i번째 날부터 얻을 수 있는 최대 수익
+dp = [0] * (n + 2)
+
+for i in range(n, 0, -1):
+    # 상담을 할 수 없는 경우
+    if i + T[i] > n + 1:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(
+            dp[i + 1],           # 상담 안 함
+            P[i] + dp[i + T[i]]  # 상담 함
+        )
+
+print(dp[1])
