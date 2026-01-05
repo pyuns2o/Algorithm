a8c81cebd3043adcffcb3eeb6e211ef10871afd4
Yunseo Park <yunseo2815@naver.com>
2026-01-05 13:14:16 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.42 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,31 +1,30 @@
 from collections import deque
 
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
-    visited = [[0]*m for _ in range(n)] # 방문 여부 초기화
-    
-    dx = [0,0,-1,1] # 좌, 우
-    dy = [-1,1,0,0] # 상, 하
+    n = len(maps) # 세로 길이
+    m = len(maps[0]) # 가로 길이
+    dist = [[-1] * m for _ in range(n)] # 거리 배열 초기화
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
     
     q = deque()
     q.append((0,0))
-    visited[0][0] = 1
+    dist[0][0] = 1
     
     while q:
-        x, y = q.popleft()
+        cur_x, cur_y = q.popleft()
         
-        for i in range(4): # 상하좌우
-            nx = x + dx[i]
-            ny = y + dy[i]
+        for dir in range(4):
+            nx = cur_x + dx[dir]
+            ny = cur_y + dy[dir]
             
-            if nx < 0 or ny <0 or nx >= n or ny >= m:
-                continue # 맵 범위 밖이면 무시
-            
-            if maps[nx][ny] == 0 or visited[nx][ny] != 0: #벽인 경우와 이미 방문한 경우 패스
+            if nx>= n or nx<0 or ny >= m or ny <0:
                 continue
                 
-            visited[nx][ny] = visited[x][y] + 1
-            q.append((nx, ny))
-    return visited[n-1][m-1] if visited[n-1][m-1] != 0 else -1
-    
+            if maps[nx][ny] == 0 or dist[nx][ny] != -1:
+                continue
+            
+            q.append((nx,ny))
+            dist[nx][ny] = dist[cur_x][cur_y] + 1
+
+    return dist[n-1][m-1]
