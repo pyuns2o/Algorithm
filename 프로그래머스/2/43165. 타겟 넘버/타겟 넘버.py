5dabe5ffcea3939847c3a2299824d41e2a660fc1
Yunseo Park <yunseo2815@naver.com>
2026-01-05 11:25:03 +0900

    [level 2] Title: 타겟 넘버, Time: 360.41 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,9 +1,12 @@
 def solution(numbers, target):
-    def dfs(index, current_sum):
-        if index == len(numbers):
-            return 1 if current_sum == target else 0
-        
-        p = dfs(index + 1, current_sum + numbers[index])
-        m = dfs(index + 1, current_sum - numbers[index])
-        return p + m
-    return dfs(0,0)
+    answer = 0
+    def dfs(idx, summ):
+        nonlocal answer
+        if idx == len(numbers):
+            if summ == target:
+                answer += 1
+            return
+        dfs(idx+1, summ + numbers[idx])
+        dfs(idx+1, summ - numbers[idx])
+    dfs(0,0)
+    return answer
