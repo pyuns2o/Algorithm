aa4677ebb9c9bb55e65dac03dc1e129623178e9d
Yunseo Park <yunseo2815@naver.com>
2026-01-05 09:37:18 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,9 @@
 def solution(array, commands):
     answer = []
-    for m in range(len(commands)):
-        i = commands[m][0]
-        j = commands[m][1]
-        k = commands[m][2]
-        temp = array[i-1:j]
-        temp.sort()
-        answer.append(temp[k-1])
+    for n in range(len(commands)):
+        i,j,k = commands[n][:]
+        tmp = array[i-1:j]
+        tmp.sort()
+        ans = tmp[k-1]
+        answer.append(ans)
     return answer
