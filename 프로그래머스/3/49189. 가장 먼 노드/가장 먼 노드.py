fbc81622a39825ecb8fa4625be1c599afee59930
Yunseo Park <yunseo2815@naver.com>
2026-01-05 15:10:38 +0900

    [level 3] Title: 가장 먼 노드, Time: 24.63 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,23 +1,28 @@
 from collections import deque
+# dist 행렬을 만들어서 거리를 다 저장한 다음에 max(dist)의 개수를 count하면 될 것으로 생각됨.
 
 def solution(n, edge):
+    # 인접 리스트 만들어주기
     graph = [[] for _ in range(n+1)]
     for a,b in edge:
         graph[a].append(b)
         graph[b].append(a)
-        
-    distance = [-1] * (n+1)
-    distance[1] = 0
     
-    queue = deque([1])
+    dist = [-1] * (n+1)
+    dist[1] = 0
     
-    while queue:
-        current = queue.popleft()
-        for neighbor in graph[current]:
-            if distance[neighbor] == -1:
-                distance[neighbor] = distance[current] + 1
-                queue.append(neighbor)
+    q = deque()
+    q.append(1)
     
-    max_dist = max(distance)
-    return distance.count(max_dist)
+    while q:
+        curr = q.popleft()
+        for nxt in graph[curr]: # 인접리스트 연결된 애들 돌아
+            # 방문한 적 없다면
+            if dist[nxt] == -1:
+                dist[nxt] = dist[curr] + 1
+                q.append(nxt)
     
+    max_dist = max(dist[1:])
+    answer = dist.count(max_dist)
+    
+    return answer
